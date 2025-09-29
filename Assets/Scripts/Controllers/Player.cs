308016e307a800d16c8c114b3d8ccff8938fa152
Vvevtor <crazynoodles64@gmail.com>
2025-09-28 23:50:47 -0400

    hopefully this merge doesn't mess anything up

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -7,6 +7,11 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
+    public Vector2 bombOffset;
+    public int numberOfTrailBombs;
+    public float bombTrailSpacing;
+    public float inDistance;
+
 
     //Vector3 inOffset = new Vector3();
     //Vector3 SpawnBombAtOffset(Vector3 inOffset); //completely clueless, no idea how to make a method. at least by myself at the beginning of class during the first exercise.
@@ -19,14 +24,66 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.B))
         {
-            SpawnBombAtOffset(new Vector3(0, 1));
+            SpawnBombAtOffset(new Vector3(bombOffset.x, bombOffset.y));
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
     }
 
-    private void SpawnBombAtOffset(Vector3 inOffset)
+    public void SpawnBombAtOffset(Vector3 inOffset)
     {
         Vector3 spawnPosition = transform.position + inOffset;
         Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
-    } 
+    }
+
+
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
+    {
+
+        
+         
+        for (int i = 0; i < inNumberOfBombs; i++)
+        {
+
+            
+            Vector3 spawnPosition = transform.position + new Vector3(0f, -(inNumberOfBombs * i) + 0.5f, 0f);
+            Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+        }
+
+
+
+
+    }
+
+    public void SpawnBombOnRandomCorner(float inDistance)
+    {
+        int location = Random.Range(1, 5);
+
+        if (location == 1)
+        {
+            Vector3 spawnPosition = transform.position + Vector3.up + Vector3.right;
+        }
+        else if (location == 2)
+        {
+            Vector3 spawnPosition = transform.position + Vector3.up + Vector3.left;
+        }
+        else if (location == 3)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(0f, -(inNumberOfBombs * i) + 0.5f, 0f);
+        }
+        else if (location == 4)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(0f, -(inNumberOfBombs * i) + 0.5f, 0f);
+        }
+
+    }
+
 }
