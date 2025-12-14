fec4d45e79d4f60b3d28f37211dbfba85a246505
Vvevtor <163864746+Vvevtor@users.noreply.github.com>
2025-12-14 05:41:15 -0500

    ROTATION MECHANIC
    rotation mechanic proposal implementation attempt, successful.

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -5,14 +5,28 @@ using UnityEngine.UIElements;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 0.5f;
     public float arrivalDistance;
     public float maxFloatDistance;
     private Vector2 asteroidDestination;
+    private int asteroidspindirection = 1;
+    private float spindeterminer;
+    private float speeddeterminer;
+    private float maniacalAsteroidSpinner;
 
     // Start is called before the first frame update
     void Start()
     {
+        spindeterminer = Random.value;
+        speeddeterminer = Random.value;
+
+        if (spindeterminer >= .5)
+        {
+            asteroidspindirection = -1 * asteroidspindirection;
+        }
+
+        speeddeterminer = speeddeterminer * 10;
+        
         asteroidDestination = (Random.insideUnitCircle);
         asteroidDestination = asteroidDestination.normalized;
         asteroidDestination = ((asteroidDestination * maxFloatDistance) + (Vector2)transform.position);
@@ -34,5 +48,21 @@ public class Asteroid : MonoBehaviour
 
             transform.position = Vector3.MoveTowards(transform.position, asteroidDestination, moveSpeed * Time.deltaTime);
         }
+
+        float fullRadians = Mathf.PI * 2;
+
+        maniacalAsteroidSpinner += ((fullRadians / speeddeterminer) * Time.deltaTime);
+
+        //zero2one += ((1 / drawingTime) * Time.deltaTime);
+        //float maniac = ((fullRadians / numberOfPowerups) * i);
+
+        Vector2 maniacalAsteroidRotation = new Vector2(Mathf.Cos(maniacalAsteroidSpinner), Mathf.Sin(maniacalAsteroidSpinner));
+
+        float TRUEMANIACALASTEROIDROTATION = Mathf.Atan2(maniacalAsteroidRotation.y, maniacalAsteroidRotation.x);
+
+        //transform.rotation.eulerAngles.z = transform.rotation.eulerAngles.z + TRUEMANIACALASTEROIDROTATION;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + TRUEMANIACALASTEROIDROTATION);
+
     }
 }
